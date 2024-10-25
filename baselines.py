70912e2cc7f1daf488fb857c135fea23e4cc6535
Luc Raszewski <lraszewski@icloud.com>
2024-10-25 22:08:09 +1100

    removed some magic numbers

--- baselines.py
+++ baselines.py
@@ -4,6 +4,9 @@ from tqdm import tqdm
 from helpers import get_distributions, results_folder, save_results, completed
 
 SAVE_DIR = '/mnt/d/results'
+NUM_CLASSES = 2
+EMBEDDING_SIZE = 768
+LABEL_INDEX = 2
 
 # get a set of random predictions
 def random_baseline(fn, test_labels):
@@ -11,8 +14,8 @@ def random_baseline(fn, test_labels):
     if completed(results, fn):
         return
 
-    probs = torch.randint(0, 2, (len(test_labels),))
-    embeds = torch.rand(len(test_labels), 768)
+    probs = torch.randint(0, NUM_CLASSES, (len(test_labels),))
+    embeds = torch.rand(len(test_labels), EMBEDDING_SIZE)
     save_results(results, fn, test_labels, probs, embeds)
 
 # get a set of majority predictions based on the train set
@@ -23,7 +26,7 @@ def majority_baseline(fn, train_labels, test_labels):
     
     majority_class = torch.mode(train_labels).values.item()
     probs = torch.full_like(test_labels, majority_class)
-    embeds = torch.full((len(test_labels),768), majority_class)
+    embeds = torch.full((len(test_labels), EMBEDDING_SIZE), majority_class)
     save_results(results, fn, test_labels, probs, embeds)
 
 # iterate through each task and generate a random and majority baseline
@@ -35,8 +38,8 @@ def baselines(test_distribution):
         train_set = task['train_set_standard']
         test_set = task['test_set_standard']
 
-        train_labels = [train_set[i][2] for i in range(len(train_set))]
-        test_labels = [test_set[i][2] for i in range(len(test_set))]
+        train_labels = [train_set[i][LABEL_INDEX] for i in range(len(train_set))]
+        test_labels = [test_set[i][LABEL_INDEX] for i in range(len(test_set))]
 
         train_labels = torch.tensor(train_labels)
         test_labels = torch.tensor(test_labels)
