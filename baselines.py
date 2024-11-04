8a48175ca43694b8dbd79214fe929ad4ddced05f
Luc Raszewski <lraszewski@icloud.com>
2024-11-05 09:57:17 +1100

    added an optimal baseline

--- baselines.py
+++ baselines.py
@@ -1,9 +1,13 @@
+import os
+import random
 import torch
+import pandas as pd
 from tqdm import tqdm
 
-from helpers import get_distributions, results_folder, save_results, completed
+from helpers import get_distributions, results_folder, save_results, completed, read_investigation
 
 SAVE_DIR = '/mnt/d/results'
+INVS_DIR = './investigations'
 NUM_CLASSES = 2
 EMBEDDING_SIZE = 768
 LABEL_INDEX = 2
@@ -29,6 +33,45 @@ def majority_baseline(fn, train_labels, test_labels):
     embeds = torch.full((len(test_labels), EMBEDDING_SIZE), majority_class)
     save_results(results, fn, test_labels, probs, embeds)
 
+def optimal_baseline(fn):
+    results = results_folder(SAVE_DIR, 'optimal_baseline')
+    if completed(results, fn):
+        return
+
+    path = os.path.join(INVS_DIR, fn)
+    data = read_investigation(path)
+
+    # recompute puppetmaster
+    puppetmaster = data[data['sock'] == 1]['user'].mode()[0]
+    labels = [0 if user == puppetmaster else 1 if sock == 1 else 2 for user, sock in zip(data['user'], data['sock'])]
+    data['label'] = labels
+    
+    # copy the same approach as in data distribution to create test set.
+    data = data.sample(frac=1, random_state=64).reset_index(drop=True)
+
+    puppetmaster_samples = data[data['label'] == 0]
+    sockpuppet_samples = data[data['label'] == 1]
+    negatives = data[data['label'] == 2]
+
+    ratio = len(puppetmaster_samples) / (len(puppetmaster_samples) + len(sockpuppet_samples))
+    split = int(len(negatives) * ratio)
+    # support_negatives = negatives.iloc[:split].reset_index(drop=True)
+    test_negatives = negatives.iloc[split:].reset_index(drop=True)
+    # support_data = pd.concat([puppetmaster_samples, support_negatives], axis=0).reset_index(drop=True)
+    test_data = pd.concat([sockpuppet_samples, test_negatives], axis=0).reset_index(drop=True)
+    # support_data['label'] = support_data['label'].map({0: 1, 1: 1, 2: 0})
+    test_data['label'] = test_data['label'].map({0: 1, 1: 1, 2: 0})
+
+    test_labels = test_data['label'].to_list()
+    probs = [label if (message != "") else random.uniform(0, 1) for label, message in zip(test_data['label'], test_data['message'])]
+    embeds = [torch.full((EMBEDDING_SIZE,), label, dtype=torch.float) if (message != "") else torch.rand(EMBEDDING_SIZE, dtype=torch.float) for label, message in zip(test_data['label'], test_data['message'])]
+
+    test_labels = torch.tensor(test_labels)
+    probs = torch.tensor(probs)
+    embeds = torch.stack(embeds)
+
+    save_results(results, fn, test_labels, probs, embeds)
+
 # iterate through each task and generate a random and majority baseline
 def baselines(test_distribution):
 
@@ -44,8 +87,9 @@ def baselines(test_distribution):
         train_labels = torch.tensor(train_labels)
         test_labels = torch.tensor(test_labels)
 
-        random_baseline(fn, test_labels)
-        majority_baseline(fn, train_labels, test_labels)
+        # random_baseline(fn, test_labels)
+        # majority_baseline(fn, train_labels, test_labels)
+        optimal_baseline(fn)
     
 
 if __name__ == '__main__':
